3acf4d904aa8933a6fc5b309d9e7a7cfd5855f2d
Yann Duplouy <yann.duplouy@inria.fr>
2019-04-12 13:01:05 +0200

    This uses C libraries, not C++ libraries…

--- src/xbt/RngStream.c
+++ src/xbt/RngStream.c
@@ -22,7 +22,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <cmath>
+#include <math.h>
 
 /*---------------------------------------------------------------------*/
 /* Private part.                                                       */
