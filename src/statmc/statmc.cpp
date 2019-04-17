38fc0ec9bae2dc849472b9d14b1a1b38e39a35dc
Yann Duplouy <yann.duplouy@inria.fr>
2019-04-17 18:53:31 +0200

    Missing header for statmc.cpp

--- src/statmc/statmc.cpp
+++ src/statmc/statmc.cpp
@@ -3,6 +3,7 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
+#include "src/statmc/statmc.hpp"
 #include "simgrid/s4u/Engine.hpp"
 #include "simgrid/simix.h"
 #include "src/simix/smx_private.hpp"
