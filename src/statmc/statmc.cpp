e076f76b2cabb628587f667f526fe107ae330f5a
Yann Duplouy <yann.duplouy@inria.fr>
2019-04-19 14:39:06 +0200

    Printing some information on the progression of the multiple simulations

--- src/statmc/statmc.cpp
+++ src/statmc/statmc.cpp
@@ -41,6 +41,8 @@ void multirun(int nbruns, const std::string& deploy, double singlesimto)
   SIMIX_set_simulationtimeout(singlesimto);
   double simulationEndTime = 0.0;
   double averageEndTime    = 0.0;
+  double lowestEndTime     = 0.0;
+  double highestEndTime    = 0.0;
   int timeouts             = 0;
   int completed            = 0;
   for (int i = 0; i < nbruns; i++) {
@@ -51,15 +53,25 @@ void multirun(int nbruns, const std::string& deploy, double singlesimto)
     try {
       SIMIX_run();
       simulationEndTime = SIMIX_get_clock();
+      if (completed == 0 || simulationEndTime < lowestEndTime) {
+        lowestEndTime = simulationEndTime;
+      }
+      if (simulationEndTime > highestEndTime) {
+        highestEndTime = simulationEndTime;
+      }
       completed += 1;
       XBT_CINFO(statmc, "Ending time (for run %i) was %f.", i, simulationEndTime);
       averageEndTime = averageEndTime * completed / (completed + 1) + simulationEndTime / (completed + 1);
     } catch (simgrid::SimulationTimeoutError& ex) {
       timeouts++;
     }
+    printf("\r%i/%i simulation(s) performed (%i failed), current estimate for average simulation time: %f. Range: [%f "
+           "; %f]",
+           i, nbruns, timeouts, averageEndTime, lowestEndTime, highestEndTime);
   }
-  printf("The average simulation time for completed runs was %f, there was %i completed runs out of %i.",
+  printf("The average simulation time for completed runs was %f, there was %i completed runs out of %i.\n",
          averageEndTime, completed, nbruns);
+  printf("The simulation times for completed runs ranged from %f to %f", lowestEndTime, highestEndTime);
 }
 
 } // namespace statmc
